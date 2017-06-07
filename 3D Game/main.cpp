a06120fc580a24c07d98fe864e0afbf94e87a1e0
Stelian <stenly232@mail.bg>
2017-06-07 19:10:44 +0100

    adding some containers

--- 3D Game/main.cpp
+++ 3D Game/main.cpp
@@ -84,7 +84,7 @@ int main() {
 
 	// Build and compile our shader program
 	Shader lightingShader("res/shaders/lighting.vs", "res/shaders/lighting.frag");
-	Shader lampShader("res/shaders/lamp.vs", "res/shaders/lamp.frag");
+	//Shader lampShader("res/shaders/lamp.vs", "res/shaders/lamp.frag");
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	GLfloat vertices[] = {
@@ -130,6 +130,20 @@ int main() {
 		0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,     1.0f,  0.0f,
 		-0.5f,  0.5f,  0.5f,    0.0f,  1.0f,  0.0f,     0.0f,  0.0f,
 		-0.5f,  0.5f, -0.5f,    0.0f,  1.0f,  0.0f,     0.0f,  1.0f
+	};
+
+	// Positions all containers
+	vec3 cubePositions[] = {
+		glm::vec3(0.0f,   0.0f,   0.0f),
+		glm::vec3(2.0f,   5.0f,   -15.0f),
+		glm::vec3(-1.5f,  -2.2f,  -2.5f),
+		glm::vec3(-3.8f,  -2.0f,  -12.3f),
+		glm::vec3(2.4f,   -0.4f,  -3.5f),
+		glm::vec3(-1.7f,  3.0f,   -7.5f),
+		glm::vec3(1.3f,   -2.0f,  -2.5f),
+		glm::vec3(1.5f,   2.0f,   -2.5f),
+		glm::vec3(1.5f,   0.2f,   -1.5f),
+		glm::vec3(-1.3f,  1.0f,   -1.5f)
 	};
 
 	// First, set the container's VAO (and VBO)
